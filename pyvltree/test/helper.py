c643e7bea429bcb658b68d8b99ba856f655a89ef
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2018-10-01 19:33:03 +0200

    Adds test helper class with method to create nodes
    In case the name of the node class changes the tests will not need
    to change.

---
+++ pyvltree/test/helper.py
@@ -0,0 +1,8 @@
+from pyvltree._avl_node import _AVLNode
+
+
+class Helper:
+
+    @staticmethod
+    def create_node(value):
+        return _AVLNode(value)
--- pyvltree/test/test_avl_tree.py
+++ pyvltree/test/test_avl_tree.py
@@ -1,6 +1,6 @@
 import unittest
+from .helper import Helper
 from pyvltree.avl_tree import AVLTree
-from pyvltree._avl_node import _AVLNode
 
 
 class TestAVLTree(unittest.TestCase):
@@ -28,23 +28,23 @@ class TestAVLTree(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     def test_delete__set_root_to_none__deleting_leaf_root(self):
-        self.tree._root = _AVLNode(1)
+        self.tree._root = Helper.create_node(1)
         self.tree.delete(1)
 
         self.assertTrue(self.tree._root is None)
 
     def test_delete__update_root__deleting_root_with_children(self):
-        self.tree._root = _AVLNode(5)
-        self.tree._root.left = _AVLNode(2)
-        self.tree._root.right = _AVLNode(7)
+        self.tree._root = Helper.create_node(5)
+        self.tree._root.left = Helper.create_node(2)
+        self.tree._root.right = Helper.create_node(7)
 
         self.tree.delete(5)
 
         self.assertEqual(self.tree._root.value, 7)
 
     def test_delete__correct_state__deleting_leaf_node(self):
-        self.tree._root = _AVLNode(10)
-        self.tree._root.left = _AVLNode(5)
+        self.tree._root = Helper.create_node(10)
+        self.tree._root.left = Helper.create_node(5)
 
         self.tree.delete(5)
 
@@ -52,9 +52,9 @@ class TestAVLTree(unittest.TestCase):
         self.assertTrue(self.tree._root.right is None)
 
     def test_delete__correct_state__deleting_node_with_one_child(self):
-        self.tree._root = _AVLNode(20)
-        self.tree._root.right = _AVLNode(50)
-        self.tree._root.right.left = _AVLNode(30)
+        self.tree._root = Helper.create_node(20)
+        self.tree._root.right = Helper.create_node(50)
+        self.tree._root.right.left = Helper.create_node(30)
 
         self.tree.delete(50)
 
@@ -63,10 +63,10 @@ class TestAVLTree(unittest.TestCase):
         self.assertTrue(self.tree._root.right.left is None)
 
     def test_delete__correct_state__deleting_node_with_two_children(self):
-        self.tree._root = _AVLNode(100)
-        self.tree._root.left = _AVLNode(50)
-        self.tree._root.left.left = _AVLNode(25)
-        self.tree._root.left.right = _AVLNode(75)
+        self.tree._root = Helper.create_node(100)
+        self.tree._root.left = Helper.create_node(50)
+        self.tree._root.left.left = Helper.create_node(25)
+        self.tree._root.left.right = Helper.create_node(75)
 
         self.tree.delete(50)
 
