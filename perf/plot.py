bf495678d66caa5555240b090ee6ad03a44b163c
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2018-10-13 15:55:40 +0200

    Refactors plot script, adds benchmark data and plots

--- perf/plot.py
+++ perf/plot.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python
 
+import sys
+import pandas
 import matplotlib
+
 matplotlib.use('agg')
+
 import matplotlib.pyplot as plt
-import pandas as pd
 
 
-data = pd.read_table('data.txt', delim_whitespace=True,
-                     names=('n', 't_s', 't_i', 't_d'))
+def main(argv):
+    data = pandas.read_table('data.txt', delim_whitespace=True,
+                             names=('n', 't_s', 't_i', 't_d'))
+    plot_linear(data)
+    plot_log(data)
 
-plt.semilogx(data.n, data.t_s, label='Search', basex=2)
-plt.semilogx(data.n, data.t_i, label='Insert', basex=2)
-plt.semilogx(data.n, data.t_d, label='Delete', basex=2)
 
-plt.xlabel('n')
-plt.ylabel('time / operation (μs)')
+def plot_linear(data):
+    subset = data.tail(20)
 
-plt.legend()
+    plt.plot(subset.n, subset.t_d, label='Delete')
+    plt.plot(subset.n, subset.t_i, label='Insert')
+    plt.plot(subset.n, subset.t_s, label='Search')
 
-plt.savefig('plot.png')
+    plt.xlabel('n')
+    plt.ylabel('time / operation (μs)')
+
+    plt.legend()
+
+    plt.savefig('plot-linear.png')
+    plt.gcf().clear()
+
+
+def plot_log(data):
+    plt.semilogx(data.n, data.t_d, label='Delete', basex=2)
+    plt.semilogx(data.n, data.t_i, label='Insert', basex=2)
+    plt.semilogx(data.n, data.t_s, label='Search', basex=2)
+
+    plt.xlabel('n')
+    plt.ylabel('time / operation (μs)')
+
+    plt.legend()
+
+    plt.savefig('plot-log.png')
+    plt.gcf().clear()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- perf/run.sh
+++ perf/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 iterations=1000
-n_pow_start=7
-n_pow_max=16
+n_pow_start=0
+n_pow_max=24
 out_file="$(\date +'%F_%T').bench"
 
 for pow in $(seq "$n_pow_start" "$n_pow_max")
