387f0c92cab64f4e9ac8673358cf13596672d1b6
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2018-10-17 03:21:24 +0200

    Bump version: 0.3.2 -> 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyvltree',
-    version='0.3.2',
+    version='0.4.0',
     author='Daniel Alm Grundström',
     author_email='daniel.alm.grundstrom@protonmail.com',
     packages=[
